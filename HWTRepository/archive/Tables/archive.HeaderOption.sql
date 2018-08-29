4b5766dd88f523a8b7a78a298266fb31825bd891
Chris Carson <ccarson@medtronic.com>
2018-08-29 14:06:57 -0500

    Delete unused tables from archive schema

--- HWTRepository/archive/Tables/archive.HeaderOption.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE
-    archive.HeaderOption(
-        HeaderID            int             NOT NULL
-      , OptionID            int             NOT NULL
-      , VersionNumber       int             NOT NULL
-      , VersionTimestamp    datetime2(7)    NOT NULL    CONSTRAINT DF_archive_HeaderOption_VersionTimestamp DEFAULT SYSDATETIME()
-      , CONSTRAINT PK_archive_HeaderOption  PRIMARY KEY CLUSTERED( HeaderID, OptionID, VersionNumber )
-);
--- HWTRepository/hwt/FileTables/hwt.ArchiveXML_Files.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE	TABLE hwt.ArchiveXML_Files 
-			AS FILETABLE 
-				FILESTREAM_ON [HWTFiles]
-			WITH
-				( 
-					FILETABLE_COLLATE_FILENAME	=	SQL_Latin1_General_CP1_CI_AS
-				  , FILETABLE_DIRECTORY 		= 	N'ArchiveXML' 
-				) 
-		;
