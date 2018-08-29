8df6806b1ad4e86719d4766777920fa9508ab152
Chris Carson <ccarson@medtronic.com>
2018-08-29 14:07:48 -0500

    Added new helper function, added new archive table because labViewStage.header is subject to update

---
+++ HWTRepository/archive/Tables/archive.labViewStage_header.sql
@@ -0,0 +1,51 @@
+﻿CREATE TABLE
+	archive.labViewStage_header
+		(
+			ID					int				NOT NULL
+		  , ResultFile			nvarchar(1000)
+		  , StartTime			nvarchar(100)
+		  , FinishTime			nvarchar(100)
+		  , TestDuration		nvarchar(100)
+		  , ProjectName			nvarchar(100)
+		  , FirmwareRev			nvarchar(100)
+		  , HardwareRev			nvarchar(100)
+		  , PartSN				nvarchar(100)
+		  , OperatorName		nvarchar(100)
+		  , TestMode			nvarchar(50)
+		  , TestStationID		nvarchar(100)
+		  , TestName			nvarchar(250)
+		  , TestConfigFile		nvarchar(400)
+		  , TestCodePathName	nvarchar(400)
+		  , TestCodeRev			nvarchar(100)
+		  , HWTSysCodeRev		nvarchar(100)
+		  , KdrivePath			nvarchar(400)
+		  , Comments			nvarchar(max)
+		  , ExternalFileInfo	nvarchar(max)
+		  , IsLegacyXML			int
+		  , VectorCount			int
+		  , CreatedDate			datetime2(3)	NOT NULL
+		  , UpdatedDate			datetime2(3)
+		  , VersionNumber		int				NOT NULL
+		  , VersionTimestamp	datetime2(3)	NOT NULL
+
+		  , CONSTRAINT	PK_labViewStage_header
+				PRIMARY KEY CLUSTERED( ID ASC, VersionNumber ASC)
+				WITH( DATA_COMPRESSION = PAGE )
+				ON [HWTTables]
+
+		)
+		ON	[HWTTables]
+		TEXTIMAGE_ON [HWTTables]
+		;
+GO
+
+CREATE INDEX
+	IX_labViewStage_header_OperatorName
+		ON labViewStage.header
+			( OperatorName ASC )
+
+		WITH( DATA_COMPRESSION = PAGE )
+		ON [HWTIndexes]
+		;
+
+
---
+++ HWTRepository/hwt/Views/hwt.vw_HeaderTag_expanded.sql
@@ -0,0 +1,26 @@
+CREATE VIEW	
+	hwt.vw_HeaderTag_expanded
+--
+--  expanded view of hwt.HeaderTag
+--
+AS
+
+  SELECT	HeaderID		=	hTag.HeaderID						
+		  , TagID           =	tag.TagID                           
+		  , TagName         =	tag.Name 
+		  , IsDeleted       =	tag.IsDeleted                       
+		  , TagTypeID       =	tType.TagTypeID                     
+		  , TagTypeName     =	tType.Name 
+		  , TagDescription	=	tag.Description 
+		  , IsPermanent     =	tType.IsPermanent                   
+		  , IsRestricted    =	tType.IsRestricted                  
+    FROM	hwt.Tag AS tag                         
+			INNER JOIN	hwt.TagType AS tType 
+					ON 	tType.TagTypeID = tag.TagTypeID 
+					
+			INNER JOIN	hwt.HeaderTag AS hTag 
+					ON tag.TagID = hTag.TagID
+			; 
+GO
+
+
